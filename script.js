7589fa050e95aa756f4effcc58f9e65efcfb7374
danielmacielbenicio <124325178+danielmacielbenicio@users.noreply.github.com>
2024-09-24 21:03:36 -0300

    atualizar arquivos

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+window.onload = () => {
+  escrever()
+}
+
 const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
@@ -7,29 +11,47 @@ avanca.forEach(button => {
 
     atual.classList.remove('ativo');
     document.getElementById(proximoPasso).classList.add('ativo');
+    escrever()
   })
 })
 
-
-
-
-
 var app = document.getElementById('app')
 
-var Typewriter = new Typewriter(app, {
+const arrText = [
+  [
+    'Quando nasci,em um planeta muito distante da Terra, meus pais me deram o nome de Dandan... ',
+    ' e desde então,estou decidido à explorar outros  planetas.',
+    ' Eu tenho a escolha de começar pelo planeta de minha mãe ou de meu pai. ',
+    'Qual devo escolher?'
+  ],
+  [
+    'Eu comecei minha jornada em Marte, e encontrei um homem chamado Leon. Ele me disse que estava em busca de ficar invisível.',
+    'E que apenas UMA FÓRMULA poderia fazer esta proeza...',
+    'A fórmula criada por Tokinho,',
+    'uma entidade cósmica que vivia aos redores de Marte'
+  ]
+]
+
+var Typewriter = new Typewriter(app, { 
   loop: false,
   delay: 75,
 });
 
-
-Typewriter
+function escrever() {
+  
+  Typewriter
   .pauseFor(1000)
-  .typeString('Quando nasci,em um planeta muito distante da Terra, meus pais me deram o nome de Dandan... ')
+  .typeString(arrText[0][0])
   .pauseFor(150)
-  .typeString(' e desde então,estou decidido à explorar outros planetas.')
+  .typeString(arrText[0][1])  
   .pauseFor(300)
-  .typeString(' Eu tenho a escolha de começar pelo planeta de minha mãe ou de meu pai. ')
+  .typeString(arrText[0][2])
   .pauseFor(300)
-  .typeString('Qual devo escolher?')
+  .typeString(arrText[0][3])
   .pauseFor(300)
   .start()
+}
+
+
+
+
